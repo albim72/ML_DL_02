b87accf58782d1ae919c58b3c4baa3f62721622a
albim72 <marcin.albiniak72@gmail.com>
2023-02-07 14:40:59 +0100

    Update pandas_fruits.py

--- DZIEN_1/pandas_fruits.py
+++ DZIEN_1/pandas_fruits.py
@@ -16,3 +16,18 @@ print(fru)
 
 fru.to_csv('fruits.csv')
 print(fru.info())
+
+unsorted_df = pd.DataFrame(np.random.randn(13,3),index=[1,4,6,2,3,5,8,9,0,7,10,11,12],columns=['b','a','c'])
+print(unsorted_df)
+
+sorted_df = unsorted_df.sort_index()
+print(sorted_df)
+
+sorted_df = unsorted_df.sort_index(ascending=False)
+print(sorted_df)
+
+sorted_df = unsorted_df.sort_index(axis=1)
+print(sorted_df)
+
+sorted_df = unsorted_df.sort_values(by='a')
+print(sorted_df)
