1d1514074ffbefb411ec0086fdeb7b27071d1d5e
albim72 <marcin.albiniak72@gmail.com>
2023-02-07 10:21:26 +0100

    Update main.py

--- DZIEN_1/MOJE_MODULY/main.py
+++ DZIEN_1/MOJE_MODULY/main.py
@@ -4,6 +4,7 @@ from dane import book,nr_filia
 from dane import book as ksiazka
 from dane import nr_filia as filia
 from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
+from klasy.cdane import CDane
 
 print("_________________________________________")
 print(nr_filia)
@@ -16,3 +17,8 @@ print(ksiazka)
 print("____________________użycie funkcji bfunkcje_____________________")
 czytaj_liste(filia)
 czytaj_slownik(ksiazka)
+
+print("____________________użycie obiektu klasy CDane_____________________")
+d = CDane(filia,ksiazka)
+d.czytaj_l()
+d.czytaj_d()
