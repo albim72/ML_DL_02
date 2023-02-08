a6c1779aee4fce041609db9627f211b069bb5dc0
albim72 <marcin.albiniak72@gmail.com>
2023-02-08 10:48:49 +0100

    Create wybor_klasyfikatora.py

---
+++ DZIEN_2/wybor_klasyfikatora.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons,make_circles,make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+h = 0.02
+
+name = [
+    "Nearest Neighbours",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Neural Network",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA"
+]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear",C=0.025),
+    SVC(gamma=2,C=1),
+    GaussianProcessClassifier(1.0*RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5,n_estimators=10,max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()
+]
+
+X,y = make_classification(n_features=2,n_redundant=0,n_informative=2,random_state=1,n_clusters_per_class=1)
+rng = np.random.RandomState(2)
+X += 2*rng.uniform(size=X.shape)
+lineary_separable=(X,y)
+
+datasets = [
+    make_moons(noise=0.3,random_state=0),
+    make_circles(noise=0.2,factor=0.5,random_state=1),
+    lineary_separable
+]
