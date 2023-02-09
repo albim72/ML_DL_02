c4888d7c40b1452325c4bb4b68b35b1b5d08a83c
albim72 <marcin.albiniak72@gmail.com>
2023-02-09 09:54:19 +0100

    Update pima.py

--- DZIEN_3/pima.py
+++ DZIEN_3/pima.py
@@ -8,3 +8,19 @@ pima = pd.read_csv("diabetes.csv",header=None,names=col_names)
 print(pima.head())
 
 feature_cols = ['pregnant','insulin','bmi','age','glucose','bp','pedigree']
+X = pima[feature_cols]
+y = pima.label
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)
+
+clf = DecisionTreeClassifier()
+
+clf = clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+
+w = metrics.accuracy_score(y_test,y_pred)
+print(f"Dokładność: {w}")
+
+
+
